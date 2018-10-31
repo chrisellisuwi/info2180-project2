f4c44843755e00d8883923d29227cd77f59f23d1
Christoph Ellis <christoph_ellis@outlook.com>
2018-10-31 21:57:30 +0000

    Added function to find empty space

--- fifteen.js
+++ fifteen.js
@@ -2,12 +2,17 @@ window.onload= function(){
     let puzzleArea = document.getElementById("puzzlearea");
     puzzleArea.style.display = "grid";
     puzzleArea.style.gridTemplateColumns = "repeat(4,auto)";
-    let puzzlePiece = document.querySelectorAll("#puzzlearea div");
+    let puzzlePiece = puzzleArea.children;
     pieceSet();
+    //puzzlePiece[14].style.gridColumn = "4/ span 1";
+    console.log(findEmpty());
     function pieceSet (){
         let i = 0;
         for (let y = 0; y < 4; y++){
             for (let x = 0; x < 4; x++){
+                //console.log(x);
+                //console.log(y);
+                if(x==3&& y==3){break;};
                 puzzlePiece[i].classList.add("puzzlepiece");
                 puzzlePiece[i].style.position = "relative";
                 puzzlePiece[i].setAttribute("id", "xy(" + x + "," + y + ")");
@@ -16,4 +21,19 @@ window.onload= function(){
             }
         }
     }
+    
+    function findEmpty (){
+        let movable = "";
+        for (let y = 0; y < 4; y++){
+            for (let x = 0; x < 4; x++){
+                if (document.getElementById(`xy(${x},${y})`) == null){
+                    movable = `xy(${x},${y})`;
+                    break;
+                } 
+            }
+        }
+        return movable;
+    }
+        
+    
 }
