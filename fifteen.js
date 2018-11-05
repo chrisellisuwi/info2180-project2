05d448ce99376099afceb21625f6421a697ae01d
chrisellis <christoph_ellis@outlook.com>
2018-11-05 04:49:09 -0600

    Changed buttons to drop menu added documentation

--- fifteen.js
+++ fifteen.js
@@ -1,28 +1,28 @@
 // Extra feature: Multiple backgrounds
 window.onload= function(){
-    let puzzleArea = document.getElementById("puzzlearea");
-    puzzleArea.style.display = "grid";
-    puzzleArea.style.gridTemplateColumns = "repeat(4,auto)";
-    let puzzlePiece = puzzleArea.children;
-    pieceSet();
-    backgrounds();
-    document.getElementById("shufflebutton").addEventListener("click", shuffle);
+    let puzzleArea = document.getElementById("puzzlearea"); //referencing the puzzle container 
+    puzzleArea.style.display = "grid"; 
+    puzzleArea.style.gridTemplateColumns = "repeat(4,auto)";    //setting the container to a 4x4 grid 
+    let puzzlePiece = puzzleArea.children;  //storing a set with all puzzle pieces
+    pieceSet();  //setting up pieces 
+    backgrounds(); //loading backgrounds
+    document.getElementById("shufflebutton").addEventListener("click", shuffle); //setting functionality for shuffle button
 
 
     function pieceSet (){
-        let i = 0;
-        for (let y = 0; y < 4; y++){
-            for (let x = 0; x < 4; x++){
-                if(x==3&& y==3){break;};
-                puzzlePiece[i].classList.add("puzzlepiece");
-                puzzlePiece[i].style.position = "relative";
-                puzzlePiece[i].setAttribute("id", "xy(" + x + "," + y + ")");
-                puzzlePiece[i].style.backgroundPosition = (-1 * 100 * x) + "px" + " " + (-1 * 100 * y) + "px";
-                puzzlePiece[i].style.gridRow = `${y + 1} / span 1`;
+        let i = 0;  //traversal counter for the puzzle piece set 
+        for (let y = 0; y < 4; y++){        //traversing the rows of the grid 
+            for (let x = 0; x < 4; x++){    //traversing the columns of the grid 
+                if(x==3&& y==3){break;};    //set so that an id isn't assigned to the empty space 
+                puzzlePiece[i].classList.add("puzzlepiece");  //adding class of "puzzlepiece" to puzzlepieces  
+                puzzlePiece[i].style.position = "relative";     // setting positions to relative to get a grid layout 
+                puzzlePiece[i].setAttribute("id", "xy(" + x + "," + y + ")");   //Setting id in the form xy(column,row)
+                puzzlePiece[i].style.backgroundPosition = (-1 * 100 * x) + "px" + " " + (-1 * 100 * y) + "px"; // setting tile backgrounds
+                puzzlePiece[i].style.gridRow = `${y + 1} / span 1`;     //setting tile positions
                 puzzlePiece[i].style.gridColumn = `${x + 1} / span 1`;
-                puzzlePiece[i].addEventListener("click",movePiece);
-                puzzlePiece[i].addEventListener("mouseover", function (){
-                    if (isMovable(this))
+                puzzlePiece[i].addEventListener("click",movePiece); //making movable tiles move when clicked 
+                puzzlePiece[i].addEventListener("mouseover", function (){   //changing appearance of movable tiles when 
+                    if (isMovable(this))                                    // mouse is over them 
                         this.classList.add("movablepiece");
                     else
                         this.classList.remove("movablepiece");
@@ -30,7 +30,7 @@ window.onload= function(){
                 i++;
             }
         }
-        setBackground(Math.floor(Math.random() * 4));
+        setBackground(Math.floor(Math.random() * 4));       //choosing a random background 
     }
     
     function findEmpty (){
@@ -49,8 +49,8 @@ window.onload= function(){
     function movePiece (){
         if (isMovable(this)){
             let empty = findEmpty();
-            let xEmp = parseInt(empty[3]);
-            let yEmp = parseInt(empty[5]);
+            let xEmp = getXCo(empty);
+            let yEmp = getYCo(empty);
             this.style.gridRow = `${yEmp + 1} / span 1`;
             this.style.gridColumn = `${xEmp + 1} / span 1`;
             this.setAttribute("id", "xy(" + xEmp + "," + yEmp + ")" );
@@ -59,8 +59,8 @@ window.onload= function(){
     
     function isMovable (piece){
         let coo = $( piece ).attr('id');
-        let xCo = parseInt(coo[3]);
-        let yCo = parseInt(coo[5]);
+        let xCo = getXCo(coo);
+        let yCo = getYCo(coo);
         let upID = document.getElementById(`xy(${xCo},${yCo-1})`);
         let downID = document.getElementById(`xy(${xCo},${yCo+1})`);
         let leftID = document.getElementById(`xy(${xCo-1},${yCo})`);
@@ -107,8 +107,8 @@ window.onload= function(){
     function shuffle(){
         for (let i = 0; i < 300; i++){
             let empty = findEmpty();
-            let xCo = parseInt(empty[3]);
-            let yCo = parseInt(empty[5]);
+            let xCo = getXCo(empty);
+            let yCo = getYCo(empty);
             let upID = document.getElementById(`xy(${xCo},${yCo-1})`);
             let downID = document.getElementById(`xy(${xCo},${yCo+1})`);
             let leftID = document.getElementById(`xy(${xCo-1},${yCo})`);
@@ -129,28 +129,8 @@ window.onload= function(){
         }
     }
 
-    function backgrounds (){
-        let bg1 = document.createElement("button");
-        let bg2 = document.createElement("button");
-        let bg3 = document.createElement("button");
-        let bg4 = document.createElement("button");
-        let bgText1 = document.createTextNode("Background 1");
-        let bgText2 = document.createTextNode("Background 2");
-        let bgText3 = document.createTextNode("Background 3");
-        let bgText4 = document.createTextNode("Background 4");   
-        bg1.appendChild(bgText1);
-        bg2.appendChild(bgText2);
-        bg3.appendChild(bgText3);
-        bg4.appendChild(bgText4);
-        document.getElementById("controls").appendChild(bg1);
-        document.getElementById("controls").appendChild(bg2);
-        document.getElementById("controls").appendChild(bg3);
-        document.getElementById("controls").appendChild(bg4);
-        bg1.addEventListener("click", function(){ setBackground(0)});
-        bg2.addEventListener("click", function(){ setBackground(1)});
-        bg3.addEventListener("click", function(){ setBackground(2)});
-        bg4.addEventListener("click", function(){ setBackground(3)});
-    }
+    function getXCo (coo) {return parseInt(coo[3]);}
+    function getYCo (coo) {return parseInt(coo[5]);}
 
     function setBackground(ind){
         if (ind == 0){
@@ -179,4 +159,41 @@ window.onload= function(){
         }
     }
 
+    function backgrounds (){
+        let menu = document.createElement("form");
+        let drop = document.createElement("select");
+        let def = document.createElement("option");
+        let defText = document.createTextNode("<select background>"); 
+        let bg1 = document.createElement("option");
+        let bgText1 = document.createTextNode("Background 1");
+        let bg2 = document.createElement("option");
+        let bgText2 = document.createTextNode("Background 2");
+        let bg3 = document.createElement("option");
+        let bgText3 = document.createTextNode("Background 3");
+        let bg4 = document.createElement("option");
+        let bgText4 = document.createTextNode("Background 4");
+        def.appendChild(defText);
+        bg1.appendChild(bgText1);
+        bg2.appendChild(bgText2);
+        bg3.appendChild(bgText3);
+        bg4.appendChild(bgText4);
+        drop.appendChild(def); 
+        drop.appendChild(bg1);
+        drop.appendChild(bg2);
+        drop.appendChild(bg3);
+        drop.appendChild(bg4);
+        menu.appendChild(drop);
+        drop.addEventListener("change", function () {
+            if (drop.selectedIndex == "1")
+                setBackground(0);
+            else if (drop.selectedIndex == "2")
+                setBackground(1);
+            else if (drop.selectedIndex == "3")
+                setBackground(2);
+            else if (drop.selectedIndex == "4")
+                setBackground(3);
+        });
+        document.getElementById("controls").appendChild(menu);
+    }
+    
 }
